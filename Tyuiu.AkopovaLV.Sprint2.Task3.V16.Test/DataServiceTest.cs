707a9b31f3b4c86078e300cedd344a41a5dad95e
AkopovaLuda <ludmilaakopova27@gmail.com>
2025-11-10 07:52:48 -1200

    Task3

---
+++ Tyuiu.AkopovaLV.Sprint2.Task3.V16.Test/DataServiceTest.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.AkopovaLV.Sprint2.Task3.V16.Lib;
+namespace Tyuiu.AkopovaLV.Sprint2.Task3.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest : IDataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 3;
+            double y = ds.Calculate(x);
+            double wait = 11;
+            Assert.AreEqual(wait, y);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double y = ds.Calculate(x);
+            double wait = 0.75;
+            Assert.AreEqual(wait, y);
+
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            DataService ds = new DataService();
+            double x = -3;
+            double y = ds.Calculate(x);
+            double wait = 2.091;
+            Assert.AreEqual(wait, y);
+
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            DataService ds = new DataService();
+            double x = -21;
+            double y = ds.Calculate(x);
+            double wait = -230.952;
+            Assert.AreEqual(wait, y);
+
+        }
+    }
+}
