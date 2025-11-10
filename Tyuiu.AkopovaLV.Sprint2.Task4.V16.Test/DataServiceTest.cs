98dfd287b285ef8d63c7183de98d3d20cbaf62a6
AkopovaLuda <ludmilaakopova27@gmail.com>
2025-11-10 07:26:33 -1200

    Task4

---
+++ Tyuiu.AkopovaLV.Sprint2.Task4.V16.Test/DataServiceTest.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.AkopovaLV.Sprint2.Task4.V16.Lib;
+namespace Tyuiu.AkopovaLV.Sprint2.Task4.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 3;
+            double y = 0;
+            double res = ds.Calculate(x, y);
+            double wait = 11;
+            Assert.AreEqual(wait, res);
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double y = 0;
+            double res = ds.Calculate(x, y);
+            double wait = 0.75;
+            Assert.AreEqual(wait, res);
+
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            DataService ds = new DataService();
+            double x = -3;
+            double y = 0;
+            double res = ds.Calculate(x, y);
+            double wait = 2.091;
+            Assert.AreEqual(wait, res);
+
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            DataService ds = new DataService();
+            double x = -21;
+            double y = 0;
+            double res = ds.Calculate(x, y);
+            double wait = -230.952;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.AkopovaLV.Sprint2.Task4.V16/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.AkopovaLV.Sprint2.Task4.V16.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double y = 0;
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнила: Акопова Л. В. | АСОиУб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема:  Оператор if – полная и короткая форма записи                     *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #29                                                             *");
+        Console.WriteLine("* Выполнила: Акопова Людмила Владимировна | АСОиУб-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с    *");
+        Console.WriteLine("* использованием вложенных оператор if-else, где пользователь вводит      *");
+        Console.WriteLine("* значение переменной X с клавиатуры. Округлить полученное значение       *");
+        Console.WriteLine("* до трех знаков после запятой;                                           *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Введите x:                                                                *");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        double res = ds.Calculate(x, y);
+        Console.WriteLine(res);
+    }
+}
