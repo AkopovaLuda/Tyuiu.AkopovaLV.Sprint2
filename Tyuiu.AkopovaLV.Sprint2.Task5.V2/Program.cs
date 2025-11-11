00840d9358cd3af79890d46c5b68f5878d58c547
AkopovaLuda <ludmilaakopova27@gmail.com>
2025-11-11 05:17:09 -1200

    Task5

---
+++ Tyuiu.AkopovaLV.Sprint2.Task5.V2/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.AkopovaLV.Sprint2.Task5.V2.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнила: Акопова Л. В. | АСОиУб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Оператор switch                                                   *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #2                                                              *");
+        Console.WriteLine("* Выполнила: Акопова Людмила Владимировна | АСОиУб-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет требуемое значение                *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите номер месяца: ");
+        int numMonth = Convert.ToInt32(Console.ReadLine());
+
+        string res;
+
+        if ((numMonth < 1) || (numMonth > 12))
+        {
+            res = "Введенно неверное значение!";
+        }
+        else
+        {
+            res = "Этот месяц попадает в пору года: " + ds.FindMonthSeason(numMonth);
+        }
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+
+        Console.WriteLine(res);
+
+        Console.ReadKey();
+    }
+}
