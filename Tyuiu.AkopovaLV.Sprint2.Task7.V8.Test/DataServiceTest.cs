b925076bf122b1f6c9e01702bd09050aebc26037
AkopovaLuda <ludmilaakopova27@gmail.com>
2025-11-11 07:07:13 -1200

    Task7

---
+++ Tyuiu.AkopovaLV.Sprint2.Task7.V8.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AkopovaLV.Sprint2.Task7.V8.Lib;
+namespace Tyuiu.AkopovaLV.Sprint2.Task7.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 5;
+            double y = 4;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
